c88c6a7cf992466787bb0d3785ff7fdc7fddd0f8
cwhinfrey <cwhinfrey@gmail.com>
2022-02-09 20:46:55 -0800

    Replace TokenLock contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -53,49 +53,58 @@ task('maketree', 'Generates a merkle airdrop tree', async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const hardhatConfig: HardhatUserConfig = {
-  solidity: '0.8.7',
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.7'
+      },
+      {
+        version: '0.7.3'
+      }
+    ]
+  },
   networks: {
     hardhat: {
       initialDate: config.UNLOCK_BEGIN,
       // tags: ['test']
       accounts: {
-        mnemonic: process.env.HOP_MNEMONIC_TESTNET,
+        mnemonic: process.env.HOP_MNEMONIC_TESTNET
       },
       mining: {
         auto: false,
         interval: [3000, 15000],
-      },
+      }
     },
     localhost: {
       url: 'http://localhost:8545',
       accounts: {
-        mnemonic: process.env.HOP_MNEMONIC_TESTNET,
-      },
+        mnemonic: process.env.HOP_MNEMONIC_TESTNET
+      }
     },
     mainnet: {
       url: process.env.RPC_ENDPOINT_MAINNET || '',
       accounts:
-        process.env.DEPLOYER_PRIVATE_KEY !== undefined ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+        process.env.DEPLOYER_PRIVATE_KEY !== undefined ? [process.env.DEPLOYER_PRIVATE_KEY] : []
     },
     goerli: {
       url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
       accounts: {
-        mnemonic: process.env.HOP_MNEMONIC_TESTNET,
-      },
+        mnemonic: process.env.HOP_MNEMONIC_TESTNET
+      }
     },
     rinkeby: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
       accounts: {
-        mnemonic: process.env.HOP_MNEMONIC_TESTNET,
-      },
+        mnemonic: process.env.HOP_MNEMONIC_TESTNET
+      }
     },
     ropsten: {
       url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
       accounts: {
-        mnemonic: process.env.HOP_MNEMONIC_TESTNET,
-      },
-    },
-  },
+        mnemonic: process.env.HOP_MNEMONIC_TESTNET
+      }
+    }
+  }
 }
 
 export default hardhatConfig
