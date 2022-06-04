3ec374f9b9ecb9b0c3ffbe6507da9c439973a4f3
cwhinfrey <cwhinfrey@gmail.com>
2022-06-03 18:13:42 -0700

    Move token and timelock address to config

--- deploy/setMerkleRoot.ts
+++ deploy/setMerkleRoot.ts
@@ -1,23 +1,20 @@
 import hre from 'hardhat'
 import config from '../config'
 
-const tokenAddress = '0x0000000000000000000000000000000000000000'
-const timelockAddress = '0x0000000000000000000000000000000000000000'
-
 export async function setMerkleRoot() {
   const tokenFactory = await hre.ethers.getContractFactory('HOPToken')
-  const token = tokenFactory.attach(tokenAddress)
+  const token = tokenFactory.attach(config.TOKEN_ADDRESS)
 
   const { network } = hre
 
-  console.log('setting merkle root: network, token, root', network.name, token.address, tree.root)
+  console.log('setting merkle root: network, token, root', network.name, token.address, config.MERKLE_ROOT)
 
   await token.setMerkleRoot(config.MERKLE_ROOT)
 
-  console.log('transferring ownership of token -> timelock', token.address, timelockAddress)
+  console.log('transferring ownership of token -> timelock', token.address, config.TIMELOCK_ADDRESS)
 
   // Transfer ownership of the token to the timelock controller
-  await token.transferOwnership(timelockAddress)
+  await token.transferOwnership(config.TIMELOCK_ADDRESS)
 
   console.log('token ownership transferred to timelock')
 }
